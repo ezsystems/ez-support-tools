8662afa30cf17b33e47314f39ab9b2f26338f535
Konrad Oboza <34310128+konradoboza@users.noreply.github.com>
2020-09-14 13:31:20 +0200

    Missing success code for the SystemInfoDumpCommand (#58)

--- Command/SystemInfoDumpCommand.php
+++ Command/SystemInfoDumpCommand.php
@@ -91,7 +91,7 @@ EOD
                 $output->writeln("  $identifier", true);
             }
 
-            return;
+            return Command::SUCCESS;
         }
 
         $outputFormatter = $this->outputFormatRegistry->getItem(
@@ -111,7 +111,9 @@ EOD
         }
 
         $output->writeln(
-             $outputFormatter->format($collectedInfoArray)
-         );
+            $outputFormatter->format($collectedInfoArray)
+        );
+
+        return Command::SUCCESS;
     }
 }
