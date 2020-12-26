cce7d3c611de57c8f55c4f0313bd4b6ca20cd068
André R <ar@ez.no>
2020-12-26 11:43:23 +0100

    Implement plugin

--- src/lib/Composer/SubscriptionPlugin.php
+++ src/lib/Composer/SubscriptionPlugin.php
@@ -9,23 +9,34 @@ declare(strict_types=1);
 namespace EzSystems\EzSupportTools\Composer;
 
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\Plugin\PostFileDownloadEvent;
-use Composer\Plugin\PreFileDownloadEvent;
+use Composer\Script\ScriptEvents;
 
 /**
- * Class SubscriptionPlugin
+ * Plugin for loading subscriction info for hints in admin UI, for trail & expiry info when relevant.
  *
- * COMPOSER_DEBUG_EVENTS=1 composer update
+ * Will only register itself if it detects that install is configerd with 'https://updates.ibexa.co' as composer repository.
+ *
+ * To debug events fired by Composer, use: COMPOSER_DEBUG_EVENTS=1 composer update
  */
-class SubscriptionPlugin implements PluginInterface
+class SubscriptionPlugin implements PluginInterface, EventSubscriberInterface
 {
+    /**
+     * @var Composer
+     */
     protected $composer;
+
     /**
      * @var IOInterface
      */
     protected $io;
+
+    /**
+     * @var bool
+     */
+    private $hasUpdatesIbexaCoRepo = false;
 
     public function activate(Composer $composer, IOInterface $io)
     {
@@ -43,39 +54,57 @@ class SubscriptionPlugin implements PluginInterface
 
     public static function getSubscribedEvents()
     {
-        return array(
-            PluginEvents::PRE_FILE_DOWNLOAD => [
-                ['onPreFileDownload', 0],
-            ],
-            // NOTE: Won't be fired if there are no downloads
-            PluginEvents::POST_FILE_DOWNLOAD => [
-                ['onPostFileDownload', 0],
-            ],
-        );
+        return [
+            ScriptEvents::PRE_INSTALL_CMD => 'checkRepositoryConfig',
+            ScriptEvents::PRE_UPDATE_CMD => 'checkRepositoryConfig',
+
+            ScriptEvents::POST_INSTALL_CMD => 'downloadSubscriptionInfo',
+            ScriptEvents::POST_UPDATE_CMD => 'downloadSubscriptionInfo',
+        ];
     }
 
-    public function onPreFileDownload(PreFileDownloadEvent $event)
+    public function checkRepositoryConfig(): void
     {
-        $url = $event->getProcessedUrl();
-        if (false !== preg_match('@^https://updates.ez.no/[^/]+/packages.json$@', $url)) {
-            $this->io->warning(
-                "updates.ez.no will be sunset by end of 2021, please move to updates.ibexa.co.\n".
-                "    See: blog post url"
-            );
+        foreach ($this->composer->getRepositoryManager()->getRepositories() as $repo) {
+            if (strpos($repo->getRepoName(), 'https://updates.ibexa.co') !== false) {
+                $this->hasUpdatesIbexaCoRepo = true;
+            }
+
+            if (strpos($repo->getRepoName(), 'https://updates.ez.no/') !== false) {
+                $this->writeWarning("WARNING: 'updates.ez.no' is deprecated, for how to use 'updates.ibexa.co' see: https://TODO");
+            } else if (1 === preg_match('@https://updates.ibexa.co/[^/]+@', $repo->getRepoName())) {
+                $this->writeWarning("WARNING: Ibexa update repository should be configured as 'https://updates.ibexa.co'");
+            }
+        }
+    }
+
+    public function downloadSubscriptionInfo(): void
+    {
+        // Skip if we don't have updates.ibexa.co repo yet, as we then probably also don't have AUTH config for it
+        if (!$this->hasUpdatesIbexaCoRepo) {
             return;
         }
 
-        $this->io->warning(
-            "Match failed"
+        !is_dir('vendor/ibexa') && mkdir('vendor/ibexa');
+
+        $this->composer->getLoop()->getHttpDownloader()->addCopy(
+            'https://updates.ibexa.co/subscription',
+            'vendor/ibexa/subscription.json'
+        );
+
+        $this->io->write(
+            "Downloading subscription info from 'https://updates.ibexa.co/subscription'",
+            true,
+            IOInterface::VERBOSE
         );
     }
 
-    public function onPostFileDownload(PostFileDownloadEvent $event)
+    private function writeWarning(string $message)
     {
-        echo $event->getUrl();
-
-        if ($protocol === 's3') {
-            // ...
+        if (method_exists($this->io, 'warning')) {
+            $this->io->warning($message);
+        } else {
+            $this->io->write($message);
         }
     }
 }
